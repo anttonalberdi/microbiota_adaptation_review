84c8aef34f2be06220bf49b3ad6a5b99dd9178d5
anttonalberdi <anttonalberdi@gmail.com>
2022-02-02 14:28:28 +0100

    PDF analysis update

--- 3-PDF_analysis.R
+++ 3-PDF_analysis.R
@@ -1,21 +1,78 @@
-library("pdftools")
-library("pdfsearch")
+library(tabulizer)
+library(pdftools)
+library(pdfsearch)
+library(stringr)
+library(stringi)
 
 setwd("/Users/anttonalberdi/github/microbiota_adaptation_review/")
 
-#Keyword list
+####
+# Define keywords
+####
 keywords = c('wild', 'fitness', 'adaptation')
 
-#Study
-doi="10.3389_fimmu.2020.585431"
+####
+# Get list of dois
+####
+doilist <- list.files(path = "papers", pattern = ".pdf",full.names=FALSE)
+doilist <- gsub(".pdf","",doilist)
 
-#Screen PDF
-pdffile <- pdftools::pdf_text(paste0("papers/",doi,".pdf"))
-key_res <- keyword_search(pdffile, keyword = keywords)
+####
+# Iterate across dois
+####
 
-#Keyword matrix
-table(key_res[1])
+for(doi in doilist){
 
-#Save extracts to table
-keyword_table <- cbind(key_res$keyword,unlist(key_res$line_text))
-write.csv(keyword_table,paste0("extracts/",doi,".csv"))
+    print(paste0("Processing paper: ",doi))
+
+    #Extract text
+    pdftext <- c()
+    tryCatch({
+        pdftext <- tabulizer::extract_text(paste0("papers/",doi,".pdf"))
+      }, error=function(e){})
+    if(length(pdftext)==0){
+    print("         ERROR")
+    }else{
+
+        #Correct errors
+        #Remove line breaks
+        pdftext <- gsub("\n"," ",pdftext)
+
+        #Change tabs to spaces
+        pdftext <- gsub("\t"," ",pdftext)
+
+        #Correct hyphens
+        pdftext <- gsub("- ","",pdftext)
+
+        #Correct multi-spaces (iterate until no double-spaces are found)
+        spacecount <- str_count(pdftext, "  ")
+
+        while(spacecount > 0){
+        pdftext <- gsub("  "," ",pdftext)
+        spacecount <- str_count(pdftext, "  ")
+        }
+
+        #Extract keywords
+        key_res <- c()
+        tryCatch({
+        key_res <- keyword_search(pdftext, keyword = keywords)
+        }, error=function(e){})
+        if(is.null(key_res)){
+          print("         ERROR")
+        }else{
+          #Write document
+          write.table(pdftext,paste0("texts/",doi,".txt"),col.names=FALSE,row.names=FALSE)
+
+          #Get keyword statistics
+          stats <- t(t(table(key_res[1])))
+          colnames(stats) <- "Counts"
+          write.csv(stats,paste0("extracts/",doi,".txt"))
+
+          #Save extracts to table
+          keyword_table <- cbind(key_res$keyword,unlist(key_res$line_text))
+          colnames(keyword_table) <- c("Keyword","Context")
+          write.csv(keyword_table,paste0("extracts/",doi,".csv"))
+
+        }
+    }
+}
